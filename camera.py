5eae97701d471944f6c6a5064bc349ac8e79e425
Thomas Nelissen <thomasnelissen@gmail.com>
2019-08-27 16:45:53 +0200

    pygame camera

--- camera.py
+++ camera.py
@@ -347,7 +347,7 @@ def CapturePicture():
     imagecounter = imagecounter + 1
     ts = time.time()
     filename = os.path.join(imagefolder, 'images', str(imagecounter)+"_"+str(ts) + '.png')
-    image = camera.get_image()
+    pygame.image.save(backgroundPicture, filename)
     #camera.capture(filename, resize=(IMAGE_WIDTH, IMAGE_HEIGHT))
     camera.stop()
     time.sleep(1)
